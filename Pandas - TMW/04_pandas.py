50396b2fdd3c51f9ade639e9e73fecbd4fe340dd
Victor Beltrão <victorbeltraoduarte@gmail.com>
2025-03-19 16:04:25 -0400

    feat/Drop duplicates e dropNA

--- Pandas - TMW/04_pandas.py
+++ Pandas - TMW/04_pandas.py
@@ -23,3 +23,15 @@ df["pontos_100"].astype(np.float32)
 df["dfCriacao"] = df["dtCriacao"].replace({"0000-00-00 00:00:00.000": "2024-02-01 09:00:00.000"})
 pd.to_datetime(df["dfCriacao"])
 # %%
+#Trabalhando com NAs
+df.dropna()
+df.dropna(how="all") #Precisa de todos os valores NA para essa linha ser excluida. Usando o Any, se houver um NA toda a linha e excluida.
+df.dropna(how="all", subset=["qtdePontos"])
+
+#Preenchendo os valores NA
+df["qtdePontos"].fillna(0)
+
+#Removendo duplicatas
+df.drop_duplicates() #mantem a primeira replica dos dados
+df.drop_duplicates(keep="last") #Mantem a ultiam replica dos dados
+df.drop_duplicates(subset=["qtdePontos"])
