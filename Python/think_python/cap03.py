969e405d1f3f9359be0bf6864e26ace72274e412
victor <victorbeltraoduarte@gmail.com>
2026-05-04 22:10:38 -0400

    feat: new exercice

---
+++ Python/think_python/cap03.py
@@ -0,0 +1,10 @@
+# %%
+""" Escreva uma função chamada triangule que receba uma string e um número inteiro e desenhe um triangulo com altura especificada, composto de multiplas copias da string."""
+
+""" Escreva uma função chamada retangle que receba uma string e dois números inteiros e desenhe um retangulo com altura e largura especificada, composto de multiplas copias da string."""
+
+""" a musica "99 Bottles of Beer" tem uma letra que se repete, mudando apenas o número de garrafas. Escreva um programa que imprima a letra da musica, para 99 garrafas, e depois para 98, e assim por diante, até chegar a 0.
+    Exemplo:
+    99 bottles of beer on the wall, 99 bottles of beer.
+    Take one down and pass it around, 98 bottles of beer on the wall.
+"""
