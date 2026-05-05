70cd03a200b1b3f111249309a7fdea6a6cae1f9e
Victor <victorbeltraoduarte@gmail.com>
2026-05-05 10:14:39 -0400

    feat: conclusão do exercicio

--- Python/think_python/cap03.py
+++ Python/think_python/cap03.py
@@ -1,10 +1,45 @@
 # %%
-""" Escreva uma função chamada triangule que receba uma string e um número inteiro e desenhe um triangulo com altura especificada, composto de multiplas copias da string."""
+""" Escreva uma função chamada triangle que receba uma string e um número inteiro
+    e desenhe um triangulo com altura especificada, composto de multiplas copias da string."""
+def triangle(word, line) -> str:
+    x = 0
+    for x in range(1, line + 1):
+        print(f'{word * x}')
 
-""" Escreva uma função chamada retangle que receba uma string e dois números inteiros e desenhe um retangulo com altura e largura especificada, composto de multiplas copias da string."""
+triangle('l', 5)
 
-""" a musica "99 Bottles of Beer" tem uma letra que se repete, mudando apenas o número de garrafas. Escreva um programa que imprima a letra da musica, para 99 garrafas, e depois para 98, e assim por diante, até chegar a 0.
+#%%
+""" Escreva uma função chamada rectangle que receba uma string e dois números inteiros 
+    e desenhe um retangulo com altura e largura especificada, composto de multiplas copias da string."""
+def rectangle(word, width, length) -> str:
+    print(word * width)
+    
+    for _ in range(length):
+        print(word * (length) + word)
+
+    if length > 1:
+        print(word * width)
+
+rectangle('H', 5, 4)
+
+#%%
+""" a musica "99 Bottles of Beer" tem uma letra que se repete, mudando apenas o número de garrafas. 
+    Escreva um programa que imprima a letra da musica, para 99 garrafas, e depois para 98, e assim por diante, até chegar a 0.
+    
     Exemplo:
-    99 bottles of beer on the wall, 99 bottles of beer.
-    Take one down and pass it around, 98 bottles of beer on the wall.
+    99 bottles of beer on the wall,
+    99 bottles of beer.
+    Take one down and pass it around, 
+    98 bottles of beer on the wall.
 """
+def bottle_verse(number):
+    for garrafas_atuais in range(number, 0, -1):
+        print(f'{garrafas_atuais} bottles of beer on the wall,')
+        print(f'{garrafas_atuais} bottles of beer.')
+        print('Take one down and pass it around,')
+        
+        new = garrafas_atuais - 1
+        print(f'{new} bottles of beer on the wall.\n')
+
+bottle_verse(99)
+# %%
